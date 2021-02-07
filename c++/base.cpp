06ba0fa1d23ed1f8463610a910218d81d6bc9d98
btea <2356281422@qq.com>
2021-02-07 16:24:25 +0800

    add c++

---
+++ c++/base.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std; // 告诉编译器使用std命名空间
+
+int main()
+{
+    cout << "hello world!"; // 会在屏幕上显示消息 "hello world!"
+    return 0;
+}
+// c++标识符内不允许出现标点字符，比如@、&和%。c++是区分大小写的编程语言。
---
+++ c++/readme.md
@@ -0,0 +1,1 @@
+[c++教程](https://www.runoob.com/cplusplus/cpp-tutorial.html)
